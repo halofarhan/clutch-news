0bcfce920b0b85da4198dbcfd3fe33173581a95c
halofarhan <farhanamirullh@gmail.com>
2024-08-04 22:54:09 +0700

    fix login

--- CMS/src/routers/index.jsx
+++ CMS/src/routers/index.jsx
@@ -1,10 +1,11 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, redirect } from "react-router-dom";
 import Login from "../views/Login";
 import BaseLayout from "../views/BaseLayout";
 import Home from "../views/Home";
 import AddForm from "../views/AddForm";
 import EditForm from "../views/EditForm";
 import AddUser from "../views/AddUser";
+import Toastify from 'toastify-js'
 
 const router = createBrowserRouter([
     {   
@@ -13,6 +14,28 @@ const router = createBrowserRouter([
     },
     {
         element: <BaseLayout />,
+        loader: () => {
+            if (!localStorage.access_token) {
+                Toastify({
+                    text: "Please login first",
+                    duration: 2000,
+                    newWindow: true,
+                    close: true,
+                    gravity: "top",
+                    position: "left",
+                    stopOnFocus: true,
+                    style: {
+                        background: "#EF4C54",
+                        color: "#17202A",
+                        boxShadow: "0 5px 10px black",
+                        fontWeight: "bold"
+                    }
+                }).showToast();
+                return redirect('/login')
+            }
+
+            return null
+        },
         children: [
             {
                 path: "/",
